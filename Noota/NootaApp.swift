4d128b34a6e49ce98fe18e5aa52c169b99a673f9
MOHAMMED SALEH <mohamedalezzi6@gmail.com>
2025-07-07 20:18:17 +0300

    Initial Commit

---
+++ Noota/NootaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NootaApp.swift
+//  Noota
+//
+//  Created by SAFAA on 7.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NootaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NootaTests/NootaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NootaTests.swift
+//  NootaTests
+//
+//  Created by SAFAA on 7.07.2025.
+//
+
+import Testing
+@testable import Noota
+
+struct NootaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
